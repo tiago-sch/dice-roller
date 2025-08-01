c8a6fbf204c7e6f3921bf68b027ea4dcfb150e98
Tiago Schmidt <tiagohammett@gmail.com>
2025-07-31 21:29:29 -0300

    fix: fix history sidebar visual

--- src/components/HistoryDrawer/index.tsx
+++ src/components/HistoryDrawer/index.tsx
@@ -13,33 +13,37 @@ const HistoryDrawer = () => {
       </div>
       <div className="drawer-side">
         <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4 flex gap-4 flex-col-reverse">
+        <ul className="bg-base-200 text-base-content min-h-full w-80 p-4 flex gap-4 flex-col-reverse">
           {rolls.map((diceRoll, index) => {
             const filteredModifiers = diceRoll.modifiers.filter(Boolean);
 
             return (
               <li key={`${index}-${diceRoll.rolled}`}>
-                <div className="stats shadow">
-                  <div className="stat">
+                <div className="stats border border-base-300 shadow hover:shadow-2xl w-full">
+                  <div className="stat max-w-72">
                     <div className="stat-title">{format(diceRoll.date, 'dd/MM/yyyy hh:mm (z)')}</div>
-                    <div className="stat-value">{diceRoll.rolled}</div>
-                    <div className="stat-desc">
-                      <span className="block underline">{diceRoll.prompt}</span>
-                      {diceRoll.rolledValues.map((rolled, i) => {
-                        const isLast = i+1 === diceRoll.rolledValues.length;
-                        const showFinalComma = !isLast || (isLast && !!filteredModifiers.length);
-                        
-                        return (
-                          <Fragment key={`rollie-${rolled.die}-${index}`}>
-                            <b>{rolled.die}</b>{' '}
-                            (<span className={rolled.crit ? 'text-green-500' : rolled.fudge ? 'text-red-500' : ''} >
-                              {rolled.rolledValue}
-                            </span>)
-                            {showFinalComma && ', '}
-                          </Fragment>
-                        )
-                      })}
-                      {filteredModifiers.join(', ')}
+                    <div className="stat-value truncate" title={`${diceRoll.rolled}`}>
+                      {diceRoll.rolled}
+                    </div>
+                    <div className="stat-desc max-w-54 text-balance" style={{ wordWrap: "break-word" }}>
+                      <span className="block underline" title={diceRoll.prompt}>{diceRoll.prompt}</span>
+                      <span>
+                        {diceRoll.rolledValues.map((rolled, i) => {
+                          const isLast = i+1 === diceRoll.rolledValues.length;
+                          const showFinalComma = !isLast || (isLast && !!filteredModifiers.length);
+                          
+                          return (
+                            <Fragment key={`rollie-${rolled.die}-${index}`}>
+                              <b>{rolled.die}</b>{' '}
+                              (<span className={rolled.crit ? 'text-green-500' : rolled.fudge ? 'text-red-500' : ''} >
+                                {rolled.rolledValue}
+                              </span>)
+                              {showFinalComma && ', '}
+                            </Fragment>
+                          )
+                        })}
+                        {filteredModifiers.join(', ')}
+                      </span>
                     </div>
                   </div>
                 </div>
